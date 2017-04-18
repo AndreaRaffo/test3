1acd84cfab6b8f83a9ba5db2982a7370da1f2138
AndreaRaffo <andrea.raffo96@hotmail.it>
2017-04-18 13:08:22 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ if(isset($_POST["name"]) && isset($_POST["password"])){
     if($good){
 		if($_POST['autologin'] == 1){
 		$hashed_password = password_hash(trim($pass), PASSWORD_DEFAULT);
-		setcookie(trim($user),$hashed_password, time() + (3600 * 24 * 30),"Cookies"); //30 days
+		setcookie(trim($user),$hashed_password, time() + (3600 * 24 * 30)); //30 days
 		}
 		$_SESSION['name'] = $_POST["name"];
 		header("Location: private.php");
